dd38c1984d99cad63ebd4fa66a5e5eba3f30ba2b
miravm <miravm@arubanetworks.com>
2021-03-01 18:42:18 +0530

    rotated_binary_search.py

---
+++ Search/rotated_binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(data, target):
+    left = 0
+    right = len(data) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        if data[middle] == target:
+            return middle
+
+        elif data[middle] > target and data[middle + 1] <= target <= data[right]:
+            left = left + 1
+        else:
+            right = right - 1
+
+    return -1
+
+
+data = [4, 5, 6, 7, 0, 1, 2]
+target = 6
+print(binary_search(data, target))
