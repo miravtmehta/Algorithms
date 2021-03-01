73fcb513176117e751c53c786e1a6096815df119
miravm <miravm@arubanetworks.com>
2021-03-01 22:09:49 +0530

    Revised rotated_binary_search.py

--- Search/rotated_binary_search.py
+++ Search/rotated_binary_search.py
@@ -6,14 +6,20 @@ def rotated_binary_search(data, target):
         if data[middle] == target:
             return middle
 
-        elif data[middle] > target and data[middle + 1] <= target <= data[right]:
-            left = left + 1
-        else:
-            right = right - 1
+        if data[middle] > target:
+            if data[middle + 1] <= target <= data[right]:
+                left = left + 1
+            else:
+                right = right - 1
+        if data[middle] < target:
+            if data[left] <= target <= data[right - 1]:
+                right = right + 1
+            else:
+                left = left + 1
 
     return -1
 
 
 data = [4, 5, 6, 7, 0, 1, 2]
-target = 6
+target = 1
 print(rotated_binary_search(data, target))
