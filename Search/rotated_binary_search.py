aca5393ce5d3cccaa6f26058e3dae7dbcfcbf0ae
miravtmehta <54740656+miravtmehta@users.noreply.github.com>
2021-04-01 13:27:26 +0530

    Fix rotated binary program

--- Search/rotated_binary_search.py
+++ Search/rotated_binary_search.py
@@ -5,19 +5,19 @@ def rotated_binary_search(nums, target):
         middle = (left + right) // 2
         if nums[middle] == target:
             return middle
-        if nums[right] >= nums[middle]:
-            if nums[middle] <= target <= nums[right]:
+        if nums[middle] <= nums[right]:
+            if nums[middle] < target <= nums[right]:
                 left = middle + 1
             else:
                 right = middle - 1
-        if nums[middle] >= nums[left]:
-            if nums[left] <= target <= nums[middle]:
+        else:
+            if nums[left] <= target < nums[middle]:
                 right = middle - 1
             else:
-                left = left + 1
+                left = middle + 1
     return -1
 
 
-data = [4, 5, 6, 7, 0, 1, 2]
+data = [0]
 target = 1
 print(rotated_binary_search(data, target))
